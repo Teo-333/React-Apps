8a52aad82af9da4ac5b749cbf9427d581438a072
Fedor Rushkevich <fedrushkevich@gmail.com>
2024-04-23 20:55:23 +0300

    (UT-F2): prefinal commit
    (UT-02): add App.js/index.js
    
    (UT-02): fix better project structure
    
    (UT-02): delete unnessesary files
    
    (UT-02): add template for future adaptive css
    
    (UT-02): fix better project structure/add spliting in components
    
    (UT-02): add first component and basic css for it (Column)
    
    (UT-02): fix css
    
    (UT-02): fix, better columns
    
    (UT-02): add Header
    
    (UT-02): add burger menu
    
    (UT-02): better css for header
    
    (UT-02): fix burger menu
    
    (UT-02): delete Navbar component
    
    (UT-02): feat shopping cart
    
    (UT-02): fix better shopping cart (css)
    
    (UT-02): fix better css & connect new font
    
    (UT-02): add main-content and css
    
    (UT-02): add Menu, MenuItem components (and css for them)
    
    (UT-02): add burger images
    
    (UT-02): better styles for Menu, MenuItem
    
    (UT-02): fix buttons and styles
    
    (UT-02): add Footer
    
    (UT-02): basic css for Footer
    
    (UT-02): fix constants
    
    (UT-02): add styles and better Footer.jsx
    
    (UT-02): beter css for footer
    
    (UT-02): better css (Main.css,Footer.css,Header.css)
    
    (UT-02): fix background
    
    (UT-02): all fixed, pre-final

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,25 +1,49 @@
+<<<<<<< HEAD
 import InstagramIcon from "../assets/images/Instagram-icon.svg";
 import TwitterIcon from "../assets/images/Twitter-icon.svg";
 import YoutubeIcon from "../assets/images/Youtube-icon.svg";
+=======
+import BurgerDreams from "../assets/img/Burger-1.png";
+import BurgerWaldo from "../assets/img/Burger-2.png";
+import BurgerCali from "../assets/img/Burger-3.png";
+import BurgerBaconBuddy from "../assets/img/Burger-4.png";
+import BurgerSpicy from "../assets/img/Burger-5.png";
+import BurgerClassic from "../assets/img/Burger-6.png";
+import InstagramLogo from "../assets/img/Instagram-icon.svg";
+import TwitterLogo from "../assets/img/Twitter-icon.svg";
+import YoutubeLogo from "../assets/img/Youtube-icon.svg";
+>>>>>>> ce36412 ((UT-02): fix better project structure)
 
 export const SOCIALLINKS = [
   {
     name: "Instagram",
     url: "https://instagram.com",
     iconName: "icon-instagram",
+<<<<<<< HEAD
     path: InstagramIcon,
+=======
+    path: InstagramLogo,
+>>>>>>> ce36412 ((UT-02): fix better project structure)
   },
   {
     name: "Twitter",
     url: "https://twitter.com",
     iconName: "icon-twitter",
+<<<<<<< HEAD
     path: TwitterIcon,
+=======
+    path: TwitterLogo,
+>>>>>>> ce36412 ((UT-02): fix better project structure)
   },
   {
     name: "YouTube",
     url: "https://youtube.com",
     iconName: "icon-youtube",
+<<<<<<< HEAD
     path: YoutubeIcon,
+=======
+    path: YoutubeLogo,
+>>>>>>> ce36412 ((UT-02): fix better project structure)
   },
 ];
 
@@ -42,7 +66,11 @@ export const BUTTONS = [
   {
     navBar: ["Home", "Menu", "Company", "Login"],
   },
+<<<<<<< HEAD
   { mainButtons: ["Dessert", "Dinner", "Breakfast"] },
+=======
+  { mainButtons: ["Desert", "Dinner", "Breakfast"] },
+>>>>>>> ce36412 ((UT-02): fix better project structure)
   {
     menubar: [
       "Burger Dreams",
@@ -55,26 +83,39 @@ export const BUTTONS = [
   },
 ];
 
+<<<<<<< HEAD
 export const TESTMENUITEMS = [
+=======
+export const MENUITEMS = [
+>>>>>>> ce36412 ((UT-02): fix better project structure)
   {
     id: 1,
     name: "Burger Dreams",
     description: "A classic burger with cheese, lettuce, and a special sauce.",
     price: "$ 9.20",
+<<<<<<< HEAD
     imageUrl: "path/to/burger-dreams",
+=======
+    imageUrl: BurgerDreams,
+>>>>>>> ce36412 ((UT-02): fix better project structure)
   },
   {
     id: 2,
     name: "Burger Waldo",
     description: "Loaded with a variety of toppings to discover.",
     price: "$ 10.00",
+<<<<<<< HEAD
     imageUrl: "path/to/burger-waldo",
+=======
+    imageUrl: BurgerWaldo,
+>>>>>>> ce36412 ((UT-02): fix better project structure)
   },
   {
     id: 3,
     name: "Burger Cali",
     description: "Topped with avocado and a sun-dried tomato spread.",
     price: "$ 8.00",
+<<<<<<< HEAD
     imageUrl: "path/to/burger-cali",
   },
   {
@@ -90,12 +131,33 @@ export const TESTMENUITEMS = [
     description: "Spiced with jalapeños and a fiery salsa for a kick.",
     price: "$ 9.20",
     imageUrl: "path/to/burger-spicy",
+=======
+    imageUrl: BurgerCali,
+  },
+  {
+    id: 3,
+    name: "Burger Bacon Buddy",
+    description: "Crispy bacon and smoky BBQ sauce atop a juicy patty.",
+    price: "$ 9.99",
+    imageUrl: BurgerBaconBuddy,
+  },
+  {
+    id: 4,
+    name: "Burger Spicy",
+    description: "Spiced with jalapeños and a fiery salsa for a kick.",
+    price: "$ 9.20",
+    imageUrl: BurgerSpicy,
+>>>>>>> ce36412 ((UT-02): fix better project structure)
   },
   {
     id: 6,
     name: "Burger Classic",
     description: "The timeless burger, perfect in its simplicity.",
     price: "$ 8.00",
+<<<<<<< HEAD
     imageUrl: "path/to/burger-classic",
+=======
+    imageUrl: BurgerClassic,
+>>>>>>> ce36412 ((UT-02): fix better project structure)
   },
 ];
