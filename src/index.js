4b07018a7f745ccba8197b87b6bc13670e90824a
Fedor Rushkevich <fedrushkevich@gmail.com>
2024-05-01 18:49:05 +0300

    (UT-05): delete commentes, cleaner code

--- src/index.js
+++ src/index.js
@@ -2,18 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./router.jsx";
-
 import "./styles/index.css";
-
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <RouterProvider router={router}>
-//   </React.StrictMode>,
-//   document.getElementById("root")
-// );
-function aa() {
-  return <div>{router}</div>;
-}
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
