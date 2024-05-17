4fe06aaa331bb0b54cff7bc3ea979868b8ed258f
Teo-333 <fedrushkevich@gmail.com>
2024-05-17 10:58:22 +0300

    (UT-M): add ErrorBoundry,Company component

--- src/App.jsx
+++ src/App.jsx
@@ -6,26 +6,32 @@ import Login from "./pages/Login/Main/Main.jsx";
 import Main from "./pages/Menu/Main/Main.jsx";
 import Home from "./pages/Home/Main/Main.jsx";
 import Error from "./pages/Error/Main/Main.jsx"
+import Company from "./pages/Company/Main/Main.jsx";
 
 import { CountProvider } from "./context/CountContext.jsx";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import ErrorBoundary from "./context/ErrorBoundry.jsx";
 
 
 function App() {
   return (
     <CountProvider>
+      <ErrorBoundary>
       <div className="container">
         <Router>
           <Header />
           <Routes>
-            <Route path="/" element={<Login />} errorElement={<Error />} />
+            <Route path="/" element={<Login />} />
             <Route path="/menu" element={<Main />} />
             <Route path="/home" element={<Home />} />
+            <Route path="/company" element={<Company />}/>
             <Route path="/login" element={<Login />} />
+            <Route path="*" element={<Error />} />
           </Routes>
           <Footer />
         </Router>
       </div>
+      </ErrorBoundary>
     </CountProvider>
   );
 }
---
+++ src/pages/Company/Main/Main.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './Main.css';  
+
+
+function Company() {
+  return (
+    <div className='company-container'>
+      There's nothing here yet
+    </div>
+  );
+}
+
+export default Company;
